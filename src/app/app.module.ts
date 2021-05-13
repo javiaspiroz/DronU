bc88747797f3abc9b835c477b307074de51262f0
Javier <60470734+javiaspiroz@users.noreply.github.com>
2021-05-13 08:49:27 +0200

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { AlquilerComponent } from './components/alquiler/alquiler.component';
+import { VentaComponent } from './components/venta/venta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProductoComponent,
+    PerfilComponent,
+    AlquilerComponent,
+    VentaComponent
   ],
   imports: [
     BrowserModule,
