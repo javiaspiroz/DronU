cf1ee68aff7b25e6844ab1051361808a642ddeb2
Javier <60470734+javiaspiroz@users.noreply.github.com>
2021-05-27 09:36:17 +0200

    inicio caruosel

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-producto',
@@ -10,6 +11,36 @@ export class ProductoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  images = ["../../../assets/images/Drones1.jpg", "../../../assets/images/iewek-gnos-ZlkRrzJl20Q-unsplash.jpg", 
+    "../../../assets/images/jaromir-kavan-e3hH6_pSk1g-unsplash.jpg", "../../../assets/images/Drones4.png"]
+
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
+
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (this.unpauseOnArrow && slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
   }
 
 }
