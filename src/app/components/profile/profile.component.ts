4fad778c60c9b48242ea616c8741d4d496ca6ccb
Javier <60470734+javiaspiroz@users.noreply.github.com>
2021-06-03 09:42:21 +0200

    primeros campos tabla

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ToolbarService, GridComponent } from '@syncfusion/ej2-angular-grids'
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
 
@@ -18,13 +19,12 @@ export class ProfileComponent implements OnInit {
     this.toolbar = ['Search'];
     this.selectOptions = { persistSelection: true };
     this.editSettings = { allowDeleting: true };
-    this.data = [{'OrderID': 10, 'CustomerName': 'HOLA1', 'OrderDate': '10/2/4', 'Freight': ''},
-    {'OrderID': 9, 'CustomerName': 'HOLA2', 'OrderDate': '10/2/6', 'Freight': 'HOLA55'},       
-    {'OrderID': 5, 'CustomerName': 'HOLA3', 'OrderDate': '10/2/8', 'Freight': 'HOLA33'},
-    {'OrderID': 6, 'CustomerName': 'HOLA4', 'OrderDate': '10/2/10', 'Freight': 'HOLA22'},
-    {'OrderID': 1, 'CustomerName': 'HOLA5', 'OrderDate': '10/2/12', 'Freight': 'HOLA11'},]
+    this.data = [{ 'OrderID': 10, 'CustomerName': 'HOLA1', 'OrderDate': '10/2/4', 'Freight': '' },
+    { 'OrderID': 9, 'CustomerName': 'HOLA2', 'OrderDate': '10/2/6', 'Freight': 'HOLA55' },
+    { 'OrderID': 5, 'CustomerName': 'HOLA3', 'OrderDate': '10/2/8', 'Freight': 'HOLA33' },
+    { 'OrderID': 6, 'CustomerName': 'HOLA4', 'OrderDate': '10/2/10', 'Freight': 'HOLA22' },
+    { 'OrderID': 1, 'CustomerName': 'HOLA5', 'OrderDate': '10/2/12', 'Freight': 'HOLA11' },]
 
-    
   }
 
 }
