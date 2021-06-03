bd6cc0d4be275eddd03e54f39388bed8056548ac
Javier <60470734+javiaspiroz@users.noreply.github.com>
2021-06-03 14:54:36 +0200

    cambios en barra de calle

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -21,15 +21,15 @@ export class ProfileComponent implements OnInit {
     this.toolbar = ['Search'];
     this.selectOptions = { persistSelection: true };
     this.editSettings = { allowDeleting: true };
-    this.data = [{ 'order': 14578, 'date': '2021/05/14', 'type': 'Compra', 'address': 'C\ Toledo 125, Madrid', 'price': 850 },
+    this.data = [{ 'order': 14578, 'date': '2021/05/14', 'type': 'Compra', 'address': 'C/ Toledo 125, Madrid', 'price': 850 },
     { 'order': 46879, 'date': '2021/05/14', 'type': 'Curso', 'address': 'No aplicable', 'price': 350 },
     { 'order': 21364, 'date': '2021/05/12', 'type': 'Alquiler', 'address': 'No aplicable', 'price': 150 },
-    { 'order': 45642, 'date': '2021/05/04', 'type': 'Compra', 'address': 'C\ Toledo 125, Madrid', 'price': 600 },
+    { 'order': 45642, 'date': '2021/05/04', 'type': 'Compra', 'address': 'C/ Toledo 125, Madrid', 'price': 600 },
     { 'order': 45675, 'date': '2021/04/28', 'type': 'Curso', 'address': 'No aplicable', 'price': 50 },
-    { 'order': 45675, 'date': '2021/04/20', 'type': 'Compra', 'address': 'C\ Toledo 125, Madrid', 'price': 850 },
+    { 'order': 45675, 'date': '2021/04/20', 'type': 'Compra', 'address': 'C/ Toledo 125, Madrid', 'price': 850 },
     { 'order': 54526, 'date': '2021/04/15', 'type': 'Evento', 'address': 'No aplicable', 'price': 0 },
     { 'order': 52514, 'date': '2021/04/03', 'type': 'Alquiler', 'address': 'No aplicable', 'price': 50 },
-    { 'order': 46231, 'date': '2021/03/26', 'type': 'Compra', 'address': 'C\ Toledo 125, Madrid', 'price': 80 },
+    { 'order': 46231, 'date': '2021/03/26', 'type': 'Compra', 'address': 'C/ Toledo 125, Madrid', 'price': 80 },
     { 'order': 13246, 'date': '2021/03/06', 'type': 'Curso', 'address': 'No aplicable', 'price': 870 },
     { 'order': 16455, 'date': '2021/02/28', 'type': 'Alquiler', 'address': 'No aplicable', 'price': 20 },
     { 'order': 98746, 'date': '2021/02/06', 'type': 'Alquiler', 'address': 'No aplicable', 'price': 213 },
